54464b85c898ff5d0268c4d873ee2f0d474f5bc1
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2024-06-08 13:47:35 +0700

    Update morse_code_tb.sv

--- Morse Code/morse_code_tb.sv
+++ Morse Code/morse_code_tb.sv
@@ -23,21 +23,21 @@ module tb_MORSE_CODE;
 
     // Outputs
     logic out;
-    logic [pCNT_WIDTH-1:0] counter_debug;
-  	logic [2:0] index_debug;
-    logic [1:0] state_debug;
-  	logic [4:0] morse_code_debug;
+   //  logic [pCNT_WIDTH-1:0] counter_debug;
+  	// logic [2:0] index_debug;
+   //  logic [1:0] state_debug;
+  	// logic [4:0] morse_code_debug;
     // Instantiate the MORSE_CODE module
     MORSE_CODE uut (
         .clk(clk),
         .en(en),
         .sel(sel),
         .rst_n(rst_n),
-        .out(out),
-        .counter_debug(counter_debug),
-        .index_debug(index_debug),
-        .state_debug(state_debug),
-        .morse_code_debug(morse_code_debug)
+        .out(out)
+        // .counter_debug(counter_debug),
+        // .index_debug(index_debug),
+        // .state_debug(state_debug),
+        // .morse_code_debug(morse_code_debug)
     );
 
     // Clock generation
